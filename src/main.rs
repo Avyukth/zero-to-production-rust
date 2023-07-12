42736b4687a89b7ffd0a848c8eac7b687d6f536f
Avyukth <makur.subhrajit@gmail.com>
2023-07-13 01:17:09 +0530

    server up and running

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, Zero to production !");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn index(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+#[actix_web::main]
+async fn main()->std::io::Result<()>{
+    HttpServer::new(||{
+        App::new()
+        .route("/", web::get().to(index))
+        .route("/{name}", web::get().to(index))
+    })
+    .bind("localhost:8080")?
+    .run()
+    .await
 }
