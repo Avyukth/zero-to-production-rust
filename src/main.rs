efb7f6fa13c680ea502f7997e1f4383e3d135549
Avyukth <makur.subhrajit@gmail.com>
2023-07-13 01:39:55 +0530

    health check done

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
 
 async fn index(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -11,8 +11,13 @@ async fn main()->std::io::Result<()>{
         App::new()
         .route("/", web::get().to(index))
         .route("/{name}", web::get().to(index))
+        .route("/health_check", web::get().to(health_check))
     })
     .bind("localhost:8080")?
     .run()
     .await
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
 }
