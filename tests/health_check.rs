0c12ca65fecaa721d1b9d6622de2fdd0cae6df90
Avyukth <makur.subhrajit@gmail.com>
2023-07-13 18:28:22 +0530

    decoupling  of port and address

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,13 +1,28 @@
-async  fn spawn_app(){
-    let server =  zero2production::run("127.0.0.1:0").expect("Failed to start server");
+use std::net::TcpListener;
+
+
+
+fn spawn_app()-> String{
+    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2production::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+    format!("http://localhost:{}", port)
 }
 
 
 
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let address:String =  spawn_app();
+    let client = reqwest::Client::new();
+    let response = client
+                                .get(&format!("{}/health_check", &address))
+                                .send()
+                                .await
+                                .expect("Failed to execute request");
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
 }
 
 
