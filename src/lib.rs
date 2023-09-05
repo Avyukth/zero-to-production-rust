354e1eac25d9ef17162c6bba1953d0ed623c9661
Avyukth <makur.subhrajit@gmail.com>
2023-09-05 12:16:23 +0530

    adding form dat

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::dev::{Payload, Server};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
 use serde::Deserialize;
 use std::net::TcpListener;
 
@@ -26,4 +26,9 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     .listen(listener)?
     .run();
     Ok(server)
+}
+
+pub trait FromRequest: Sized {
+    type Error = Into<actix_web::Error>;
+    async fn from_request(req: &HttpRequest, Payload: &mut Payload) -> Result<Self, Self::Error>;
 }
